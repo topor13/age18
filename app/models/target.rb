8fc3c1732639f7e852de4ca2f9f09ed594fbc7a9
doniv <donivrecord@gmail.com>
2014-09-03 18:42:41 +0800

    Upd

--- app/models/target.rb
+++ app/models/target.rb
@@ -2,12 +2,10 @@ class Target < ActiveRecord::Base
   ## Relations
   has_many :photos
 
+  ## Variables
+  GENDER = %w(male female)
+
   ## Validates
-  validates :gender, :inclusion => { :in => ['male', 'female'] }
-  validates :name, presence: true
-  validates_inclusion_of :dob, :in => Date.civil(1980, 1, 1)..Date.today,
-    :message => "must be between 1980 and now"
-
-
-
+  validates :gender, inclusion: { in: GENDER }
+  validates :name, :dob, presence: true
 end
